05a5427e53bc435f5420ffc70c5e5db14c049679
BucOder <bucoder@outlook.com>
2014-01-10 19:06:23 +0100

    Init sources

---
+++ src/com/bucoder/conway/Main.java
@@ -0,0 +1,19 @@
+package com.bucoder.conway;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.SlickException;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Game game = new Game("Game of life");
+		try {
+			AppGameContainer app = new AppGameContainer(game);
+			app.setVSync(true);
+			app.setShowFPS(false);
+			app.start();
+		} catch (SlickException e) {
+			e.printStackTrace();
+		}
+	}
+}
